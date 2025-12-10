a3950d3d939c0cb13f89802e7e38ec793a6b22f4
Rosa Mayila <128403959+rosbotmay@users.noreply.github.com>
2025-12-10 13:16:40 +0100

    website repo refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/ada-2025-project-adastraperaspera/adastraperaspera.io/',
+  base: '/ada-2025-project-adastraperaspera/',
   server: {
     host: "::",
     port: 8080,
